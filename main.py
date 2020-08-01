73296d7d196d0bd1c463ed4618a7ab4aa3339e6c
pawnlord <scruffduff321@gmail.com>
2020-08-01 14:51:21 +0000

    Add save with ^S

--- main.py
+++ main.py
@@ -26,9 +26,29 @@ def terminal_size():
 def clear():
 	print("\033[H\033[J", end='');
 
+
+def save(filename, text_list):
+	text_str = ""
+	for s in text_list:
+		text_str += s + '\n'
+	w, h = terminal_size()
+	gotoxy(1,h-2);
+	for i in range(w):
+		print('\033[30;47m \033[0m', end='')
 	
-
-
+	gotoxy(1,h-2);
+	command = input("\033[30;47mSave file?[Y/n] ")
+	if command == '' or command.lower()[0] == 'y':
+		if filename == "untitled":
+			for i in range(w):
+				print('\033[30;47m \033[0m', end='')
+			
+			gotoxy(1,h-2);
+			filename = input("\033[30;47mSave as: ")
+			
+		with open(filename, 'w') as f:
+			f.write(text_str)
+	print("\033[0m")
 text_str = ""
 
 script_name = os.path.basename(__file__)
@@ -68,22 +88,36 @@ starting_line = 0
 ending_line = h
 cursor_offset = 0
 
+HEADER_FOOTER_SIZE = 5
+HEADER_SIZE = 3
+FOOTER_SIZE = 2
+
 try:
 	while True:
 		clear()
+		gotoxy(1,1);
+		print("Vomer IDE - by pawnlord - " + filename)
 		gotoxy(1,2);
 		
 		cursor_offset = 0
 		w, h = terminal_size()
-		ending_line = starting_line+h-3
+		ending_line = starting_line+h-HEADER_FOOTER_SIZE
 		
 		for i in range(w):
-			print('\033[31;47m-\033[0m', end='')
+			print('\033[30;47m-\033[0m', end='')
 		
 		for s in range(starting_line, min(len(text), ending_line)):
 			print(text[s])
 		
-		gotoxy(cursor.x+1, cursor.y+3-starting_line)
+		gotoxy(1,h-FOOTER_SIZE);
+		for i in range(w):
+			print('\033[30;47m \033[0m', end='')
+		
+		gotoxy(1,h-FOOTER_SIZE);
+		
+		print("\033[30;47m3-ESC: escape. ^S- save\033[0m");
+		
+		gotoxy(cursor.x+1, cursor.y+HEADER_SIZE-starting_line)
 		
 		c = getch()
 		if ord(c) == 27:
@@ -116,13 +150,17 @@ try:
 			text.insert(cursor.y, nl_text)
 			cursor.y+=1
 			cursor.x=0 
-		elif ord(c) == 127:
-			if cursor.x == 0 and cursor.y != 0:
+		elif ord(c) == 19:
+			save(filename, text)
+		elif ord(c) == 127:	     
+			if cursor.x == 0 and cursor.y > 0:
 				line_text = text[cursor.y][:]
 				text = text[:cursor.y] + text[cursor.y+1:]
 				cursor.y -= 1
 				cursor.x = len(text[cursor.y])
 				text[cursor.y] += line_text
+			elif cursor.x and cursor.y <= 0:
+				pass
 			else:
 				text[cursor.y] = text[cursor.y][:cursor.x-1] + text[cursor.y][cursor.x:]
 				cursor.x-=1
@@ -143,7 +181,7 @@ try:
 				cursor.x=len(text[cursor.y])
 			else:
 				cursor.x = 0
-		if cursor.y > ending_line:
+		if cursor.y > ending_line-1:
 			starting_line+=1
 			ending_line+=1
 		if cursor.y < starting_line:
